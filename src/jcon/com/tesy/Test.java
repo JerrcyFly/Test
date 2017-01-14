ebffd85c8184a9bbb2a5fffdb0d7dfc4b4ecaedb
JerrcyFly <525176261@qq.com>
2017-01-14 17:41:59 +0800

    aaa

---
+++ src/jcon/com/tesy/Test.java
@@ -0,0 +1,16 @@
+package jcon.com.tesy;
+
+public class Test {
+
+	public static void test(){
+		System.out.println("hello");
+	}
+	
+	public void test11(){
+		
+	}
+	
+	public static void main(String[] args) {
+		test();
+	}
+}
